ee4f3ae8f7f63c78feb5bebd64bbbb66aca7b94e
An10113 <antranthanh1011@gmail.com>
2023-02-06 00:10:03 +0700

    4

--- js/classes.js
+++ js/classes.js
@@ -134,4 +134,45 @@ class Fighter extends sprite{
           this.isAttacking = false
       },100)
   }
+  switchSprite(sprite) {
+  switch (sprite) {
+    case 'idle':
+      if (this.image !== this.sprites.idle.image) {
+        this.image = this.sprites.idle.image
+        this.framesMax = this.sprites.idle.framesMax
+        this.framesCurrent = 0
+      }
+      break
+    case 'run':
+      if (this.image !== this.sprites.run.image) {
+        this.image = this.sprites.run.image
+        this.framesMax = this.sprites.run.framesMax
+        this.framesCurrent = 0
+      }
+      break
+    case 'jump':
+      if (this.image !== this.sprites.jump.image) {
+        this.image = this.sprites.jump.image
+        this.framesMax = this.sprites.jump.framesMax
+        this.framesCurrent = 0
+      }
+      break
+
+    case 'fall':
+      if (this.image !== this.sprites.fall.image) {
+        this.image = this.sprites.fall.image
+        this.framesMax = this.sprites.fall.framesMax
+        this.framesCurrent = 0
+      }
+      break
+
+    case 'attack1':
+      if (this.image !== this.sprites.attack1.image) {
+        this.image = this.sprites.attack1.image
+        this.framesMax = this.sprites.attack1.framesMax
+        this.framesCurrent = 0
+      }
+      break
+    }
+  }
 }
